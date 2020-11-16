7f7813e6d6d3b7c99942f3894e3c76427e0e6c2d
Tanner Percival <percivaltanner@gmail.com>
2020-11-16 13:20:28 -0800

    validation coming

---
+++ BlazorMovies/BlazorMovies/Shared/Entities/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorMovies.Shared.Entities
+{
+    public class Genre
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
