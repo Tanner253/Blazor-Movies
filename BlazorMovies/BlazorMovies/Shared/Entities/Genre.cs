976bfc3c52b1c018d3c9d4a85f22b7e209a5341b
Tanner Percival <percivaltanner@gmail.com>
2020-11-16 14:20:13 -0800

    data annotations and form validation

--- BlazorMovies/BlazorMovies/Shared/Entities/Genre.cs
+++ BlazorMovies/BlazorMovies/Shared/Entities/Genre.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace BlazorMovies.Shared.Entities
@@ -7,6 +8,7 @@ namespace BlazorMovies.Shared.Entities
     public class Genre
     {
         public int ID { get; set; }
+        [Required(ErrorMessage ="This field is required")]
         public string Name { get; set; }
     }
 }
